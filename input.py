136d8e72354f80614049cc7f2251fa6818155024
Nirmal Prakash <nirmalp2@illinois.edu>
2018-03-03 18:37:07 -0600

    Parse Word Change

--- input.py
+++ input.py
@@ -6,6 +6,8 @@ class Input:
 		pass
 
 	def parse_words(self, a):
+		mess = Message()
+
 		# Split words into list
 		words = a.split()
 
@@ -15,33 +17,33 @@ class Input:
 
 		# Move North
 		if "NORTH" in words:
-			return(1)
+			mess.code = 1
 
 		# Move South
 		elif "SOUTH" in words:
-			return(2)
+			mess.code = 2
 
 		# Move East
 		elif "EAST" in words:
-			return(3)
+			mess.code = 3
 
 		# Move West
 		elif "WEST" in words:
-			return(4)
+			mess.code = 4
 
 		# User wants to know location
 		elif "LOOK" in words:
-			return(5)
+			mess.code = 5
 		
 		# User command not understood
 		else:
-			return(0)
+			mess.code = 0
+
+		return mess
 
 	def update(self, upd):
 		# Read from command line
 		a = input()
-		mess = Message()
-		mess.code = self.parse_words(a)
 
 		# Send instructions to update
-		upd.messages.append(mess)
+		upd.messages.append(self.parse_words(a))
