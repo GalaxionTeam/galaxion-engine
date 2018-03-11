38931d0db591bc3aa52c3b4521cd94c7f8cf6104
Eric <eordonez822@gmail.com>
2018-03-11 16:43:37 -0500

    fixed indentations

--- message.py
+++ message.py
@@ -1,6 +1,6 @@
 class Message:
 	def __init__(self, code=0, message="", debug="", args=[]):
 		self.code = code
-        self.message = message
-        self.debug = debug
-        self.args = args
+		self.message = message
+		self.debug = debug
+		self.args = args
