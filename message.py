0041d709df65570ed19fc1388a770efd4bbcf496
Eric <eordonez822@gmail.com>
2018-03-11 16:41:00 -0500

    Message init

--- message.py
+++ message.py
@@ -1,3 +1,6 @@
 class Message:
 	def __init__(self, code=0, message="", debug="", args=[]):
-		pass
+		self.code = code
+        self.message = message
+        self.debug = debug
+        self.args = args
