a5be398bb6e8057d36b91878dfa193697736fa3d
Nirmal Prakash <nirmalp2@illinois.edu>
2018-03-11 17:36:58 -0500

    fixed capitalization

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 # Class that holds player info
 class Player:
 	def __init__(self, x_location = 0, y_location = 0):
-		self.x_Location = 0
-		self.y_Location = 0
+		self.x_location = 0
+		self.y_location = 0
